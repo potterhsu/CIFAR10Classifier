3f8ada0b83008722b8a19f82b72172d6b25f7ec4
Poter <poter.hsu@coretronic.com>
2017-04-16 15:42:44 +0800

    Updated method of reading image

--- convert_to_tfrecords.py
+++ convert_to_tfrecords.py
@@ -32,7 +32,7 @@ class ExampleReader(object):
         """
         if self._example_pointer == self._num_examples:
             return None
-        image = self._images[self._example_pointer].reshape([32, 32, 3], order='F').transpose(1, 0, 2).tostring()
+        image = self._images[self._example_pointer].reshape([32, 32, 3]).tostring()
         label = self._labels[self._example_pointer]
         self._example_pointer += 1
 
